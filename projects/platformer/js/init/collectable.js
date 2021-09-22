b65d1e18797a4984c5f5f9e09bc5b3c34a0a5d96
jaygraham1 <jagraham01@outlook.com>
2021-09-22 16:54:02 +0000

    add cannons to game

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -37,9 +37,10 @@
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
         // example: 
-        createCollectable(type.steve, 200, 170, 6, 0.7);
+        createCollectable(type.steve, 200, 350);
         createCollectable(type.kennedi, 850);
         createCollectable(type.max, 850, 275);
+        createCollectable(type.db, 200, 100);
         
         
         
